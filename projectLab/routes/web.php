e0f80be5dfad4da57cdd630985314222ea223eb4
AntWort21 <67149821+AntWort21@users.noreply.github.com>
2021-10-09 23:05:49 +0700

    added testing branch

--- projectLab/routes/web.php
+++ projectLab/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+//Route::get('/', [PagesController::class, 'index']);
+Route::get('/dashboard', [UserController::class, 'index']);
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
