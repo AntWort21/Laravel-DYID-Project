61473f446cadb4e2dcb9153ca63378391a716ec7
AntWort21 <67149821+AntWort21@users.noreply.github.com>
2021-10-10 18:44:37 +0700

    added UserController with login() and register() + header & footer template + headerfooter.css

--- DYID/routes/web.php
+++ DYID/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/register', [UserController::class, 'register']);
+// Route::get('/login', [UserController::class, 'login']);
 
 Route::get('/', function () {
     return view('welcome');
