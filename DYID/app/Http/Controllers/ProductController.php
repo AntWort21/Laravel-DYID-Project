6efd81146f29f345d1290fdbf05dd2d86403d8b3
AntWort21 <67149821+AntWort21@users.noreply.github.com>
2021-11-12 16:14:50 +0700

    revamped newproduct page

--- DYID/app/Http/Controllers/ProductController.php
+++ DYID/app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -15,12 +16,17 @@ class ProductController extends Controller
         return view('newproduct');
     }
     public function insertProduct(Request $request){
+
+        $image = $request->file('image');
+
+
         $product = new Product();
         $product->product_name = $request->name;
         $product->product_description = $request->description;
         $product->product_price = $request->price;
         $product->product_categoryid = $request->category;
-        $product->product_image = $request->image;
+        $product->product_image = $image->getClientOriginalName();
+        Storage::putFileAs('public/productImage', $image, $image->getClientOriginalName());
         $product->save();
         return redirect('mainpage');
     }
