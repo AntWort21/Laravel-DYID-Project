7dc2bd8719d41978f9b66b4bee6c3110f29993cc
Daryl <daryl@binus.ac.id>
2021-12-11 19:45:21 +0700

    Updated Seeders

--- DYID/database/seeders/ProductSeeder.php
+++ DYID/database/seeders/ProductSeeder.php
@@ -15,67 +15,60 @@ class ProductSeeder extends Seeder
     public function run()
     {
         Product::create([
-            'name' => 'A',
-            'description' => 'ini A',
-            'price' => '10000',
-            'image' => 'image.jpg',
+            'name' => 'Iphone 13',
+            'description' => 'Apple s newest model in the market with the newest features and a slightly different camera design',
+            'price' => '18000000',
+            'image' => 'Apple_Phone.jpg',
             'category_id' => '1'
         ]);
         Product::create([
-            'name' => 'B',
-            'description' => 'ini B',
-            'price' => '10000',
-            'image' => 'image.jpg',
+            'name' => 'Samsung Galaxy Fold 3',
+            'description' => 'Samsung s newest model with flexible screen leading to the phone being able to be folded into a bigger screen',
+            'price' => '14000000',
+            'image' => 'Samsung_Phone.jpg',
             'category_id' => '1'
         ]);
         Product::create([
-            'name' => 'C',
-            'description' => 'ini C',
-            'price' => '10000',
-            'image' => 'image.jpg',
-            'category_id' => '1'
+            'name' => 'LG Fridge 420',
+            'description' => 'LG Fridge Model 420 smart fridge with built in freezer and a see through glass screen with LED lights',
+            'price' => '20000000',
+            'image' => 'LG_Fridge.jpg',
+            'category_id' => '2'
         ]);
         Product::create([
-            'name' => 'D',
-            'description' => 'ini D',
-            'price' => '10000',
-            'image' => 'image.jpg',
-            'category_id' => '1'
+            'name' => 'Samsung Fridge Flower Edition',
+            'description' => 'Samsung Limited Edition Flower crest edition 2020. Strong, light, and able to go as low as 10C degrees',
+            'price' => '19500000',
+            'image' => 'Samsung_Fridge.jpg',
+            'category_id' => '2'
         ]);
         Product::create([
-            'name' => 'E',
-            'description' => 'ini E',
-            'price' => '10000',
-            'image' => 'image.jpg',
-            'category_id' => '1'
+            'name' => 'Toshiba Fridge Black 370',
+            'description' => 'Toshiba Model 370 in black. Released in 2018 with multi fan cooling system able to freeze anything inside',
+            'price' => '10000000',
+            'image' => 'Toshiba_Fridge.jpg',
+            'category_id' => '2'
         ]);
         Product::create([
-            'name' => 'F',
-            'description' => 'ini F',
-            'price' => '10000',
-            'image' => 'image.jpg',
-            'category_id' => '1'
+            'name' => 'Philip 4100 TV',
+            'description' => 'Philip Television model 4100. Released in 2021 December. Incredible Quality with three year insurance',
+            'price' => '26000000',
+            'image' => 'Philip_TV.jpg',
+            'category_id' => '3'
         ]);
         Product::create([
-            'name' => 'G',
-            'description' => 'ini G',
-            'price' => '10000',
-            'image' => 'image.jpg',
-            'category_id' => '1'
+            'name' => 'Samsung QLED TV 4K',
+            'description' => 'Samsung QLED Television. Released in 2021 and able to support 4K. Smart Bluetooth features',
+            'price' => '22500000',
+            'image' => 'Samsung_TV.jpg',
+            'category_id' => '3'
         ]);
         Product::create([
-            'name' => 'H',
-            'description' => 'ini H',
-            'price' => '10000',
-            'image' => 'image.jpg',
-            'category_id' => '1'
-        ]);
-        Product::create([
-            'name' => 'I',
-            'description' => 'ini I',
-            'price' => '10000',
-            'image' => 'image.jpg',
-            'category_id' => '1'
+            'name' => 'Sony X85J 50 Inch TV',
+            'description' => 'Sony model X85J. Supports Ultra HD LED, 4K, Smart Television. 120Hz and high radiant colors',
+            'price' => '23000000',
+            'image' => 'Sony_TV.jpg',
+            'category_id' => '3'
         ]);
     }
 }
